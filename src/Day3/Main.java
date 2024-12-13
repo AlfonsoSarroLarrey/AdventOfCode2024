eb4e9217e8c34243aa6e58a60153c7826c1c1ebf
AlfonsoSarroLarrey <alfonsosarrolarrey@gmail.com>
2024-12-13 23:10:11 +0100

    Exercise 3 Part 2 solution

--- src/Day3/Main.java
+++ src/Day3/Main.java
@@ -2,41 +2,63 @@ package Day3;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
-import java.util.regex.MatchResult;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Stream;
 
 public class Main {
     static final boolean NUM1 = true;
     static final boolean NUM2 = false;
-
+    static final boolean ENABLED = true;
+    static final boolean DISABLED = false;
 
     public static void main(String[] args) {
-
         Main main = new Main();
-        long total_mul_sum = main.readCorruptProgram("./src/resources/InputDay3.txt");
+        System.out.println("Which part will be used to check the multiplications?");
+        Scanner in = new Scanner(System.in);
+
+        String partStr = in.nextLine();
+        int part = partStr.charAt(0) - '0';
+
+        in.close();
+
+        long total_mul_sum = main.readCorruptProgram("./src/resources/InputDay3.txt", part);
         System.out.println("The total sum of all multiplications is: " + total_mul_sum);
     }
 
-    private long readCorruptProgram(String fileName) {
+    private long readCorruptProgram(String fileName, int part) {
         try {
             File file = new File(fileName);
             Scanner scanner = new Scanner(file);
             //We define the regex to find all instances of "mul(<num_3dig>,<num_3dig>)"
-            Pattern regex = Pattern.compile("mul\\([0-9]{1,3},[0-9]{1,3}\\)");
-            Matcher matcher;
+            Pattern regex = Pattern.compile("mul\\([0-9]{1,3},[0-9]{1,3}\\)|don't\\(\\)|do\\(\\)");
+
+            Matcher matcherRegex;
+
+            boolean status = ENABLED;
 
             long mul_sum = 0;
 
             while(scanner.hasNextLine()) {
-                matcher = regex.matcher(scanner.nextLine());
-                while(matcher.find()) {
-                    mul_sum = mul_sum + calcMultiplication(matcher.group());
+                matcherRegex = regex.matcher(scanner.nextLine());
+
+                while(matcherRegex.find()) {
+                    if(matcherRegex.group().equals("don't()") && part == 2) {
+                        status = DISABLED;
+                    }
+                    else if (matcherRegex.group().equals("do()") && part == 2) {
+                        status = ENABLED;
+                    }
+                    else {
+                        if(status == ENABLED) {
+                            mul_sum = mul_sum + calcMultiplication(matcherRegex.group());
+                        }
+                    }
                 }
             }
+
+            scanner.close();
 
             return mul_sum;
 
